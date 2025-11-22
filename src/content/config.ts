bf877eba1baa1a57e37cdc86c19dceb931a68f6c
Ali Umed <aliumed844@gmail.com>
2025-11-23 01:05:16 +0300

    feature: create blog route to show blogs

--- src/content/config.ts
+++ src/content/config.ts
@@ -21,6 +21,30 @@ const postsCollection = defineCollection({
 		weight: z.number().default(0),
 	}),
 });
+
+const blogSchema = {
+  title: z.string(),
+  published: z.date(),
+  updated: z.date().optional(),
+  draft: z.boolean().optional().default(false),
+  description: z.string().optional().default(""),
+  image: z.string().optional().default(""),
+  tags: z.array(z.string()).optional().default([]),
+  category: z.string().optional().nullable().default(""),
+  lang: z.string().optional().default(""),
+  prevTitle: z.string().default(""),
+  prevSlug: z.string().default(""),
+  nextTitle: z.string().default(""),
+  nextSlug: z.string().default(""),
+  pinned: z.boolean().optional(),
+  weight: z.number().default(0),
+};
+
+const blogsCollection = defineCollection({
+  schema: z.object(blogSchema),
+});
+
 export const collections = {
-	posts: postsCollection,
+  posts: postsCollection,
+  blogs: blogsCollection,
 };
